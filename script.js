d98ddd24f619ff5076d0780de3d809d1b010f127
Rose <roseday871@gmail.com>
2026-03-04 12:55:39 +0000

    Initial task manager app

---
+++ script.js
@@ -0,0 +1,19 @@
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function() {
+    const taskText = taskInput.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.addEventListener("click", function() {
+        li.remove();
+    });
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+});
